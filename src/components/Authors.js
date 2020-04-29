6f3ce2d8d42a7f01cdf71026ca3902434d3dd750
ElMayo <mic.l.meier@gmail.com>
2020-04-29 18:22:55 +0200

    exercise 8.8 authors view - solved

--- src/components/Authors.js
+++ src/components/Authors.js
@@ -1,11 +1,28 @@
-  
-import React from 'react'
+import React from "react";
+import { gql, useQuery } from "@apollo/client";
+
+const ALL_AUTHORS = gql`
+  query {
+    allAuthors {
+      name
+      born
+      bookCount
+    }
+  }
+`;
 
 const Authors = (props) => {
-  if (!props.show) {
-    return null
+  const result = useQuery(ALL_AUTHORS);
+
+  if (result.loading) {
+    return <div>loading...</div>;
   }
-  const authors = []
+
+  if (!props.show) {
+    return null;
+  }
+
+  const authors = result.data.allAuthors;
 
   return (
     <div>
@@ -14,25 +31,20 @@ const Authors = (props) => {
         <tbody>
           <tr>
             <th></th>
-            <th>
-              born
-            </th>
-            <th>
-              books
-            </th>
+            <th>born</th>
+            <th>books</th>
           </tr>
-          {authors.map(a =>
+          {authors.map((a) => (
             <tr key={a.name}>
               <td>{a.name}</td>
               <td>{a.born}</td>
               <td>{a.bookCount}</td>
             </tr>
-          )}
+          ))}
         </tbody>
       </table>
-
     </div>
-  )
-}
+  );
+};
 
-export default Authors
+export default Authors;
