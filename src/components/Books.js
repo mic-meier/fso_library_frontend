bd1e6b3deda2490912923af6569bb9e05a87b1e1
ElMayo <mic.l.meier@gmail.com>
2020-04-29 19:15:20 +0200

    exercise 8.9 books view - solved

--- src/components/Books.js
+++ src/components/Books.js
@@ -1,11 +1,25 @@
-import React from 'react'
+import React from "react";
+import { gql, useQuery } from "@apollo/client";
+
+const ALL_BOOKS = gql`
+  query {
+    allBooks {
+      title
+      published
+      author
+      id
+    }
+  }
+`;
 
 const Books = (props) => {
+  const results = useQuery(ALL_BOOKS);
+
   if (!props.show) {
-    return null
+    return null;
   }
 
-  const books = []
+  const books = results.data.allBooks;
 
   return (
     <div>
@@ -15,24 +29,20 @@ const Books = (props) => {
         <tbody>
           <tr>
             <th></th>
-            <th>
-              author
-            </th>
-            <th>
-              published
-            </th>
+            <th>author</th>
+            <th>published</th>
           </tr>
-          {books.map(a =>
+          {books.map((a) => (
             <tr key={a.title}>
               <td>{a.title}</td>
               <td>{a.author}</td>
               <td>{a.published}</td>
             </tr>
-          )}
+          ))}
         </tbody>
       </table>
     </div>
-  )
-}
+  );
+};
 
-export default Books
+export default Books;
