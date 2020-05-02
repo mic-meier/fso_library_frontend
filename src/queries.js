b48c47e66c5a9560a45d8412c145880e652037f0
ElMayo <mic.l.meier@gmail.com>
2020-05-02 10:26:02 +0200

    missed files

---
+++ src/queries.js
@@ -0,0 +1,65 @@
+import { gql } from "@apollo/client";
+
+export const ALL_BOOKS = gql`
+  query {
+    allBooks {
+      title
+      published
+      author {
+        name
+      }
+      id
+    }
+  }
+`;
+
+export const ALL_AUTHORS = gql`
+  query {
+    allAuthors {
+      name
+      born
+      bookCount
+    }
+  }
+`;
+
+export const CREATE_BOOK = gql`
+  mutation createBook(
+    $title: String!
+    $author: String!
+    $published: Int!
+    $genres: [String!]!
+  ) {
+    addBook(
+      title: $title
+      author: $author
+      published: $published
+      genres: $genres
+    ) {
+      title
+      author {
+        name
+      }
+      published
+      genres
+    }
+  }
+`;
+
+export const EDIT_BIRTHYEAR = gql`
+  mutation editAuthor($name: String!, $setBornTo: Int!) {
+    editAuthor(name: $name, setBornTo: $setBornTo) {
+      name
+      born
+      bookCount
+    }
+  }
+`;
+
+export const LOGIN = gql`
+  mutation login($username: String!, $password: String!) {
+    login(username: $username, password: $password) {
+      value
+    }
+  }
+`;
